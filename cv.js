9d13b0f725dbf0dd21317d113f0f11dedaa6cbe2
Alisa <apykhova@gmail.com>
2021-09-19 22:08:39 +0300

    feat: added smooth anchor links

--- cv.js
+++ cv.js
@@ -3,3 +3,36 @@ $(document).ready(function () {
         $('.header__burger, .menu, .header__burger::after').toggleClass('someActive');
     });
 }); 
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    
+    const blockID = anchor.getAttribute('href').substr(1);
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  });
+}
+// const line1 = "1. valid. checked. +10";
+// const line2 = "2. semantic. more than 10 different tags. +20"
+// const line3 = "3. many styles there are. +10"
+// const line4 = "4. is centered, backgroud color is there. +10"
+// const line5 = "5. is adaptive. spent most f the time on this one. +10"
+// const line6 = ""
+// const line7 =
+// const line8 =
+// const line9 =
+// const line10 =
+// const line11 =
+// const line12 =
+// const line13 =
+// const line14 =
+// const line15 =
+// const line16 =
+// console.log(
+//     "Self-check:\n 1. valid. checked. +10 \n 2."
+// )
